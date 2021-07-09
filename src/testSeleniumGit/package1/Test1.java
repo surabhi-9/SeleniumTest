dc0fd8fe248396e8f4c0f28a643f21263060af5d
surabhi-9 <rathisurabhi45@gmail.com>
2021-07-09 08:06:08 +0530

    puched

---
+++ src/testSeleniumGit/package1/Test1.java
@@ -0,0 +1,11 @@
+package testSeleniumGit.package1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+		System.out.println("1.1");
+	}
+
+}
---
+++ src/testSeleniumGit/package2/Test2.java
@@ -0,0 +1,12 @@
+package testSeleniumGit.package2;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+		System.out.println("1.1");
+
+	}
+
+}
